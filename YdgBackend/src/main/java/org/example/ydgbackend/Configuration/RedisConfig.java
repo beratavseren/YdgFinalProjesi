15b045dbed932856f28556733d3d6aa41e3aff4d
beratavseren <musaberatavseren@gmail.com>
2026-01-01 22:57:08 +0300

    webhook deneme6

--- YdgBackend/src/main/java/org/example/ydgbackend/Configuration/RedisConfig.java
+++ YdgBackend/src/main/java/org/example/ydgbackend/Configuration/RedisConfig.java
@@ -6,7 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-//wbhook deneme5
+//wbhook deneme6
 @Configuration
 public class RedisConfig {
 
