b8cbdb3cdf05552c6ec54144abac901aad615123
beratavseren <musaberatavseren@gmail.com>
2026-01-02 03:01:22 +0300

    ee test5

--- YdgBackend/src/main/java/org/example/ydgbackend/Configuration/WebConfig.java
+++ YdgBackend/src/main/java/org/example/ydgbackend/Configuration/WebConfig.java
@@ -7,4 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
